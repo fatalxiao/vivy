27b3727ba8e454cbe4a27352307aec493acf3fcd
liangxiaojun <fatalxiao@163.com>
2021-07-09 11:15:19 +0800

    update

--- examples/counter/src/modules/Counter/containers/Counter.js
+++ examples/counter/src/modules/Counter/containers/Counter.js
@@ -2,52 +2,52 @@
  * @file Root.js
  */
 
-import React from 'react';
+import React, {useCallback} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
-
-// Component
-import {NavLink} from 'react-router-dom';
-
-// Vendors
-import {renderRoutes} from 'react-router-config';
 
 // Styles
 import './Counter.scss';
 
 const Counter = ({
-    route, menu
-}) => (
-    <div className="root">
+    value,
+    dispatch
+}) => {
 
-        <div className="menu">
-            <h2>Module Root</h2>
-            <ul>
-                {
-                    menu.map((item, index) =>
-                        <NavLink key={index}
-                                 to={item.route}>
-                            <li>
-                                {item.name}
-                            </li>
-                        </NavLink>
-                    )
-                }
-            </ul>
+    const handlePlus = useCallback(() => {
+        dispatch({
+            type: 'counter/plus'
+        });
+    }, [
+        dispatch
+    ]);
+
+    const handleMinus = useCallback(() => {
+        dispatch({
+            type: 'counter/minus'
+        });
+    }, [
+        dispatch
+    ]);
+
+    return (
+        <div className="counter">
+
+            Current value: {value}
+
+            <button onClick={handlePlus}>Plus</button>
+            <button onClick={handleMinus}>Minus</button>
+
         </div>
+    );
 
-        <div className="content">
-            {renderRoutes(route.routes)}
-        </div>
-
-    </div>
-);
+};
 
 Counter.propTypes = {
-    route: PropTypes.object,
-    menu: PropTypes.array
+    value: PropTypes.number,
+    dispatch: PropTypes.func
 };
 
 export default connect(state => ({
-    menu: state.root
+    value: state.counter
 }))(Counter);
