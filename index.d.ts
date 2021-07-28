e66cc41a45b4d292a91d961dbfd46456722af018
liangxiaojun <fatalxiao@163.com>
2021-07-28 13:56:16 +0800

    update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,45 @@
+import {
+    Reducer, Middleware, MiddlewareAPI, Action, Store
+} from 'redux';
+
 export interface Plugin {
 
 }
 
-export interface VivyStore {
+export interface VivyStore extends Store {
+    originStore?: Store,
+    plugins?: Plugin[],
+    registerReducer: (nameSpace?: string, reducer?: Reducer) => void,
+    registerReducers: (reducer?: Reducer) => void,
+    unregisterReducer: (nameSpace?: string) => void
+    unregisterReducers: (nameSpacesOrReducers?: string[] | Reducer[]) => void,
+    registerModel: (model?: VivyModel) => void,
+    registerModels: (models?: VivyModel[]) => void,
+    unregisterModel: (nameSpaceOrModel?: string | VivyModel) => void,
+    unregisterModels: (nameSpacesOrModels?: string[] | VivyModel[]) => void
+}
 
+export interface VivyModelAction {
+    (action?: Action): (api?: MiddlewareAPI<any>) => any
+}
+
+export interface VivyModelActions {
+    [key: string]: VivyModelAction
+}
+
+export interface VivyModelReducer {
+    (state?: any, action?: Action): any
+}
+
+export interface VivyModelReducers {
+    [key: string]: VivyModelReducer
 }
 
 export interface VivyModel {
-
+    nameSpace?: string,
+    state?: any,
+    actions?: VivyModelActions,
+    reducers?: VivyModelReducers
 }
 
 export interface VivyOption {
@@ -20,7 +52,7 @@ export interface VivyOption {
     /**
      * Register extra middlewares to Vivy
      */
-    extraMiddlewares?: []
+    extraMiddlewares?: Middleware[]
 
 }
 
@@ -49,20 +81,66 @@ export interface VivyInstance {
 
 }
 
-export function registerReducer(store?: VivyStore, nameSpace?: string, reducer?: Function): void
+/**
+ * Register reducer to Vivy
+ * @param store
+ * @param nameSpace
+ * @param reducer
+ */
+export function registerReducer(store?: VivyStore, nameSpace?: string, reducer?: Reducer): void
 
-export function registerReducers(store?: VivyStore, reducer?: Function): void
+/**
+ * Register reducers to Vivy
+ * @param store
+ * @param reducer
+ */
+export function registerReducers(store?: VivyStore, reducer?: Reducer): void
 
+/**
+ * Unregister reducer
+ * @param store
+ * @param nameSpace
+ */
 export function unregisterReducer(store?: VivyStore, nameSpace?: string): void
 
-export function unregisterReducers(store?: VivyStore, nameSpacesOrReducers?: string | Function): void
+/**
+ * Unregister reducers
+ * @param store
+ * @param nameSpacesOrReducers
+ */
+export function unregisterReducers(store?: VivyStore, nameSpacesOrReducers?: string[] | Reducer[]): void
 
+/**
+ * Register model to Vivy
+ * @param store
+ * @param model
+ */
 export function registerModel(store?: VivyStore, model?: VivyModel): void
 
+/**
+ * Register models to Vivy
+ * @param store
+ * @param models
+ */
 export function registerModels(store?: VivyStore, models?: VivyModel[]): void
 
+/**
+ * Unregister model
+ * @param store
+ * @param nameSpaceOrModel
+ */
 export function unregisterModel(store?: VivyStore, nameSpaceOrModel?: string | VivyModel): void
 
+/**
+ * Unregister models
+ * @param store
+ * @param nameSpacesOrModels
+ */
 export function unregisterModels(store?: VivyStore, nameSpacesOrModels?: string[] | VivyModel[]): void
 
+/**
+ * Create Vivy Instance
+ * @param opts
+ * @constructor
+ */
 export default function Vivy(opts?: VivyOption): VivyInstance;
