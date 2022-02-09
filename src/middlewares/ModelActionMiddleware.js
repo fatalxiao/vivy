c3a35eaa16db2dd95cd42d2538c837fe1aa96133
liangxiaojun <fatalxiao@163.com>
2022-02-09 15:55:54 +0800

    update vivy

--- src/middlewares/ModelActionMiddleware.js
+++ src/middlewares/ModelActionMiddleware.js
@@ -23,7 +23,7 @@ export default function createModelActionMiddleware() {
 
             // match type in asyncActions
             if (asyncActions?.hasOwnProperty(action?.type)) {
-                return asyncActions[action.type]?.(action)?.(dispatch, getState);
+                asyncActions[action.type]?.(action)?.(dispatch, getState);
             }
 
             return next(action);
