42ca8d1f2e1f8065c035436c0d36fa68fab50698
liangxiaojun <fatalxiao@163.com>
2022-02-14 15:23:24 +0800

    update babel rc

--- .babelrc.js
+++ .babelrc.js
@@ -2,44 +2,40 @@
  * @file .babelrc.js
  */
 
-const presets = [
-    [
-        '@babel/preset-env',
-        {
-            modules: false,
-            useBuiltIns: 'usage',
-            corejs: 3
-        }
-    ]
-];
-const commonPlugins = [
-    ['@babel/plugin-proposal-decorators', {legacy: true}],
-    '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-proposal-do-expressions',
-    '@babel/plugin-proposal-export-default-from',
-    '@babel/plugin-proposal-export-namespace-from',
-    '@babel/plugin-proposal-function-bind',
-    '@babel/plugin-proposal-function-sent',
-    '@babel/plugin-proposal-json-strings',
-    '@babel/plugin-proposal-logical-assignment-operators',
-    '@babel/plugin-proposal-nullish-coalescing-operator',
-    '@babel/plugin-proposal-numeric-separator',
-    '@babel/plugin-proposal-optional-chaining',
-    ['@babel/plugin-proposal-pipeline-operator', {proposal: 'minimal'}],
-    '@babel/plugin-proposal-throw-expressions',
-    '@babel/plugin-syntax-dynamic-import',
-    '@babel/plugin-syntax-import-meta',
-    '@babel/plugin-transform-runtime'
-];
-const packageConfig = {
-    presets,
-    plugins: commonPlugins
-};
-
 module.exports = {
     'env': {
 
-        'development': packageConfig,
+        'development': {
+            presets: [
+                [
+                    '@babel/preset-env',
+                    {
+                        modules: false,
+                        useBuiltIns: 'usage',
+                        corejs: 3
+                    }
+                ]
+            ],
+            plugins: [
+                ['@babel/plugin-proposal-decorators', {legacy: true}],
+                '@babel/plugin-proposal-class-properties',
+                '@babel/plugin-proposal-do-expressions',
+                '@babel/plugin-proposal-export-default-from',
+                '@babel/plugin-proposal-export-namespace-from',
+                '@babel/plugin-proposal-function-bind',
+                '@babel/plugin-proposal-function-sent',
+                '@babel/plugin-proposal-json-strings',
+                '@babel/plugin-proposal-logical-assignment-operators',
+                '@babel/plugin-proposal-nullish-coalescing-operator',
+                '@babel/plugin-proposal-numeric-separator',
+                '@babel/plugin-proposal-optional-chaining',
+                ['@babel/plugin-proposal-pipeline-operator', {proposal: 'minimal'}],
+                '@babel/plugin-proposal-throw-expressions',
+                '@babel/plugin-syntax-dynamic-import',
+                '@babel/plugin-syntax-import-meta',
+                '@babel/plugin-transform-runtime'
+            ]
+        },
 
         'test': {
             'presets': [
