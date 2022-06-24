b899ee6fb017668e2c02b0a8308fded56d9677b3
liangxiaojun <fatalxiao@163.com>
2022-06-24 15:26:29 +0800

    update BuildInModelReducers

--- src/reducers/BuildInModelReducers.js
+++ src/reducers/BuildInModelReducers.js
@@ -4,6 +4,12 @@
 
 export default {
 
+    /**
+     * Update model state
+     * @param state
+     * @param nextState
+     * @returns {*}
+     */
     setState: (state, {nextState}) => {
 
         if (typeof nextState === 'function') {
