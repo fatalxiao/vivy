0bef6f6f704a37e9507e65cc69047666da86163c
liangxiaojun <fatalxiao@163.com>
2023-03-08 11:24:07 +0800

    update ts

--- gulpfile.js
+++ gulpfile.js
@@ -6,8 +6,9 @@
 
 const gulp = require('gulp');
 const del = require('delete');
+const merge = require('merge2');
 const ts = require('gulp-typescript');
-const tsProject = ts.createProject('./tsconfig.json');
+const tsProject = ts.createProject('tsconfig.json');
 
 gulp.task('clean', () =>
     del([
@@ -15,10 +16,19 @@ gulp.task('clean', () =>
     ])
 );
 
-gulp.task('build', () =>
-    gulp.src('./src/**/*.js')
-        .pipe(tsProject())
-        .pipe(gulp.dest('./dist'))
-);
+gulp.task('build', () => {
+
+    const tsResult = gulp.src('./src/**/*.ts')
+                         .pipe(tsProject({
+                             declaration: true
+                         }));
+    // .pipe(gulp.dest('dist'))
+
+    return merge([
+        tsResult.dts.pipe(gulp.dest(__dirname)),
+        tsResult.js.pipe(gulp.dest('dist'))
+    ]);
+
+});
 
 gulp.task('default', gulp.series('clean', 'build'));
